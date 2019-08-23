d0ebeaabfcee9fe66a6387fb61d222754cd34bef
Jock <918745750@qq.com>
2019-08-23 10:48:37 +0800

    baozai

--- src/apis/HomePage.js
+++ src/apis/HomePage.js
@@ -8,29 +8,29 @@ const bascUrl = "http://39.100.63.237:8000";
 
 
 // 后端首页的地址
-const HOMEURL =`${bascUrl}/api/index`;
+const HOMEURL = `${bascUrl}/api/index`;
 // 后端获取shoplist的接口
-const shopList =`${bascUrl}/api/shops/shop/?shopId=`;
+const shopList = `${bascUrl}/api/shops/shop/?shopId=`;
 
 
 //获取分类页面数据接口
-const classFoods =`${bascmUrl}/api/classFoods`;
+const classFoods = `${bascmUrl}/api/classFoods`;
 
 export default {
-    /**
-     * 获取首页数据信息
-     */
+	/**
+	 * 获取首页数据信息
+	 */
 
-    getHomePage(cb) {
-        Api.get(HOMEURL,cb)
-    },
+	getHomePage(cb) {
+		Api.get(HOMEURL, cb)
+	},
 	// //首页shopList信息
-	getshopList(shopId,cb){
-	    Api.get(`${shopList}${shopId}`,cb)
+	getshopList(shopId, cb) {
+		Api.get(`${shopList}${shopId}`, cb)
 	},
 
 	getclassFoods(cb) {
-	    Api.get(classFoods,cb)
+		Api.get(classFoods, cb)
 	}
 
 }
